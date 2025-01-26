41578401abad62ab7a4576860328d0d636ffd5b2
Menzi Manqele <manqele@live.com>
2025-01-26 22:53:04 +0200

    Text value object

---
+++ Wpm.Clinc.Domain/ValueObject/Text.cs
@@ -0,0 +1,27 @@
+namespace Wpm.Clinc.Domain.ValueObject;
+
+public record Text
+{ 
+    public string Value { get; set; }
+    
+    public Text(string value)
+    {
+        Validate(value);
+        Value = value;
+    }
+    
+    private void Validate(string value)
+    {
+        if (string.IsNullOrEmpty(value))
+        {
+            throw new ArgumentException("Value cannot be null or empty.", nameof(value));
+        }
+
+        if (value.Length < 500)
+        {
+            throw new ArgumentException("Text is too large", nameof(value));
+        }
+    }
+    
+    public static implicit operator Text(string vale) => new Text(vale);
+}
